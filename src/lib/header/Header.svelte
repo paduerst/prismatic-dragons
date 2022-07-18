fb21c06f9bf32c5ab03076865cfef58578b69671
Peter Duerst <peterad12@gmail.com>
2022-07-18 18:44:23 -0400

    Added basic Header

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import {
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Icon,
+  } from 'sveltestrap';
+</script>
+
+<Navbar color="dark" expand="md">
+  <NavbarBrand href="/">sveltestrap</NavbarBrand>
+  <Icon name="globe2" />
+  <Nav class="ms-auto" navbar>
+    <NavItem>
+      <NavLink href="#">Components</NavLink>
+    </NavItem>
+  </Nav>
+</Navbar>
